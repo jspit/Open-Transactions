88ad8fac4074dcb172d51eb6959abd38421f1ab3
Cameron Garnham <da2ce7@gmail.com>
2012-06-13 16:51:15 +1000

    removed one macro, and replaced with inline functions, much neater now

--- include/otserver/OTServer.h
+++ include/otserver/OTServer.h
@@ -142,6 +142,8 @@
 
 #include "OTCron.h"
 
+#include "simpleini/SimpleIni.h"
+
 class OTMessage;
 class OTClientConnection;
 //class OTAccount;
@@ -198,7 +200,21 @@ class OTServer
 	// -------------------------------------------------------------------------------------------------------------
 	// From server.cfg:
 	//
+
+	static bool LogSettingChange(const char * szCategory,const char * szOption,const char * szValue);
+	static bool LogSettingChange(const char * szCategory,const char * szOption,long lValue);
+	static bool LogBoolSettingChange(const char * szCategory,const char * szOption,bool bValue);
 	
+	static OTString StringFill(const char * szString,int iLength,const char * szAppend);
+	static OTString StringFill(const char * szString,int iLength);
+
+	static bool CheckSetConfig(CSimpleIniA *pini, const char * szSection, const char * szKey, const char * szDefault,OTString & out_strResult);
+	static bool CheckSetConfig(CSimpleIniA *pini, const char * szSection, const char * szKey, long iDefault,long & out_lResult);
+	static bool CheckSetBoolConfig(CSimpleIniA *pini, const char * szSection, const char * szKey, bool bDefault,bool & out_bResult);
+
+	static bool SetConfigOptionBool(CSimpleIniA * pini,const char * szSection, const char * szKey, bool bVariableName);
+
+
     static int			__heartbeat_no_requests;	// The number of client requests that will be processed per heartbeat.
     static int			__heartbeat_ms_between_beats; // number of ms between each heartbeat.
 	
